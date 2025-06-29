6d01fc87a3f1e9b6ee8fed676786da775607bf93
Ambrea27-tingz <ambreawilliams97@gmail.com>
2025-06-29 13:01:15 -0400

    Intro documation for the program

---
+++ Lab9_awilliams#1.py
@@ -0,0 +1,9 @@
+"""Name: Ambrea Williams
+   
+   Date: 06/29/2025
+
+   Unit 9: Lab 9 - Coin Toss Game
+
+   Description: This program counts the frequency of each word in a text file. 
+   An let's the user choose a filename to read. 
+   """
